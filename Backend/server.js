b62fe623879f8cc7719e6cb8eff8dccadb12a91f
raviswamiii <raviswamisamsung@gmail.com>
2025-01-25 22:36:25 +0530

    databaseconnection

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const databaseConnection = require("./config/mongodb");
 const app = express();
+require("dotenv").config();
+
+
+databaseConnection();
 
 app.listen(3000, () => {
     console.log("Server is running...")

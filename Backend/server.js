1f47f1709e30dff26dde151ac2929e0c856322f7
raviswamiii <raviswamisamsung@gmail.com>
2025-01-25 22:54:32 +0530

    configuration

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const databaseConnection = require("./config/mongodb");
 const app = express();
-require("dotenv").config();
+const dotenv = require("dotenv");
 
-
+dotenv.config();
 databaseConnection();
 
 app.listen(3000, () => {

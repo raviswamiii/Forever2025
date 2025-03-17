ba1bba079f912018e25df90b463f31f657a61594
raviswamiii <raviswamisamsung@gmail.com>
2025-03-17 14:20:25 +0530

    port

--- Backend/server.js
+++ Backend/server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const databaseConnection = require("./config/mongodb");
 const app = express();
 const dotenv = require("dotenv");
+const port = process.env.PORT || 3000
 
 dotenv.config();
 databaseConnection();
 
-app.listen(3000, () => {
-    console.log("Server is running...")
+app.listen(port, () => {
+    console.log("Server is running on port: " + port)
 })
